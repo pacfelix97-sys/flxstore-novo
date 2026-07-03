b4d886017ec4d507ecc1bd62edffa8e6f8dc4188
pacfelix97-sys <pacfelix97@gmail.com>
2026-07-02 21:39:28 -0300

    Add metadata and root layout to app

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: "FlxStore",
+  description: "Minha loja",
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
